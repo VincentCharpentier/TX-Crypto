229ebb74e1c30c01a29f248d75989c44c3eeba66
VincentCharpentier <animous.man.vc@gmail.com>
2014-02-22 16:12:38 +0100

    Add a constant file

---
+++ src/tx/model/Constantes.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package tx.model;
+
+/**
+ * Classes regroupant des constantes d'ordre général.
+ * @author vincetn
+ */
+public final class Constantes {
+
+    /**
+     * Nombre de lettres dans l'alphabet. (26)
+     */
+    public static final Integer ALPHABET_SIZE = 26;
+
+    /**
+     * Constructeur privée pour empécher l'instanciation.
+     */
+    private Constantes() { }
+}
